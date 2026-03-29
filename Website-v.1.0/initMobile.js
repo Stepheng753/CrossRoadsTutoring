654bc6fba922af70f993157b9240514b97209353
Stephen Giang <StephenG753@Gmail.com>
2026-03-28 22:12:23 -0700

    removed previous versions

--- Website-v.1.0/initMobile.js
+++
@@ -1,5 +0,0 @@
-let link = document.getElementsByTagName('link')[0];
-
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-	link.href = './mobile-style.css';
-}
